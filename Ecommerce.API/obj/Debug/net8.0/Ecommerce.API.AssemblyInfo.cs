ff2e198af461827b5093cd5aafb994a9c3cb975c
diogosalvadorb <diogosalvadorb@gmail.com>
2025-08-27 18:44:37 -0300

    .

--- Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
+++ Ecommerce.API/obj/Debug/net8.0/Ecommerce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d406e1afa04c8eb45dedac1f4c33e05039d7f945")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
